28e020b69fea7ebdca21e454845dd6a09e0ca3cc
Mateus Eduardo <medu2710@gmail.com>
2023-04-22 00:59:52 -0300

    feat: exibir fotos cadastradas

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -14,24 +14,75 @@ import { useParams } from "react-router-dom";
 
 // Redux
 import { getUserDetails } from "../../slices/userSlice";
+import {
+  publishPhoto,
+  resetMessage,
+  getUserPhotos,
+} from "../../slices/photoSlice";
 
 const Profile = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
 
-  // photo
+  const {
+    photos,
+    loading: loadingPhoto,
+    message: messagePhoto,
+    error: errorPhoto,
+  } = useSelector((state) => state.photo);
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+
+  // New form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   // Load user data
   useEffect(() => {
     dispatch(getUserDetails(id));
+    dispatch(getUserPhotos(id));
+    console.log("cade " + photos);
   }, [dispatch, id]);
+
+  // Publish a new photo
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    const photoData = {
+      title,
+      image,
+    };
+
+    // build form data
+    const formData = new FormData();
+
+    Object.keys(photoData).forEach((key) =>
+      formData.append(key, photoData[key])
+    );
+
+    dispatch(publishPhoto(formData));
+
+    setTitle("");
+
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000);
+  };
+
+  const handleFile = (e) => {
+    const image = e.target.files[0];
+
+    setImage(image);
+  };
 
   if (loading) {
     return <p>Carregado...</p>;
   }
-  
+
   return (
     <div id="profile">
       <div className="profile-header">
@@ -41,6 +92,64 @@ const Profile = () => {
         <div className="profile-description">
           <h2>{user.name}</h2>
           <p>{user.bio}</p>
+        </div>
+      </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Título para a foto:</span>
+                <input
+                  type="text"
+                  placeholder="Insira um título"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""}
+                />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" onChange={handleFile} />
+              </label>
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto && (
+                <input type="submit" disabled value="Aguarde..." />
+              )}
+            </form>
+          </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
+        </>
+      )}
+      <div className="user-photos">
+        <h2>Fotos publicadas:</h2>
+        <div className="photos-container">
+          {photos &&
+            photos.map((photo) => (
+              <div className="photo" key={photo._id}>
+                {photo.image && (
+                  <img
+                    src={`${uploads}/photos/${photo.image}`}
+                    alt={photo.title}
+                  />
+                )}
+                {id === userAuth._id ? (
+                  <div className="actions">
+                    <Link to={`/photos/${photo._id}`}>
+                      <BsFillEyeFill />
+                    </Link>
+                    <BsPencilFill />
+                    <BsXLg />
+                  </div>
+                ) : (
+                  <Link className="btn" to={`/photos/${photo._id}`}>
+                    Ver
+                  </Link>
+                )}
+              </div>
+            ))}
+          {photos.length === 0 && <p>Ainda não há fotos publicadas</p>}
         </div>
       </div>
     </div>
